2229ba3526ced33c8260e6fd11dc7b6f1a2a7287
Lovic Gauthier <lovic@ariake-nct.ac.jp>
2017-05-11 07:49:03 +0900

    Added declaration and instantiation of high-level system types.

--- lib/HDLRuby/hruby_high.rb
+++ lib/HDLRuby/hruby_high.rb
@@ -5,21 +5,74 @@ require "HDLRuby.rb"
 #######################################################
 module HDLRuby::High
 
+    # # Service methods: should not be used directly.
+
+    # # Instantiate hardware type +htype+ to an instance of class +i_class+ with
+    # # name +i_name+ and possible +args+ arguments.
+    # def self.instantiate(htype,i_class,i_name,*args)
+    #     # Create the eigen type.
+    #     eigen = htype.class.new("")
+    #     eigen.instance_eval(*args,htype.instance_proc) if htype.instance_proc
+    #     # Create the instance.
+    #     return i_class.new(i_name,eigen)
+    # end
+
     ##
     # Module providing high-level features to hardware types.
     module HType
         # The proc used for instantiating the hardware type.
-        attr_reader :instantiater
+        attr_reader :instance_proc
+        
+        # The instantiation target class.
+        attr_reader :instance_class
 
-        # Sets the proc for instantiating the hardware type to +block+.
-        def instantiater=(block)
-            # Checks and sets the proc.
-            unless block.is_a?(Proc)
-                raise "Invalid class for an instantiater: #{block.class}."
-            end
-            @instantiater = instantiater
+        # # Sets the proc for instantiating the hardware type to +block+.
+        # def instance_proc=(block)
+        #     # Checks and sets the proc.
+        #     unless block.is_a?(Proc) then
+        #         raise "Invalid class for an instantiation proc: #{block.class}."
+        #     end
+        #     @instance_proc = block
+        # end
+
+        # # Sets the instantiation target class to +klass+.
+        # def instance_class=(klass)
+        #     # Checks and sets the class.
+        #     unless klass.is_a?(Class) then
+        #         raise "Invalid class for an instantiation class: #{klass.class}."
+        #     end
+        #     @instance_class = klass
+        # end
+
+        # Instantiate the hardware type to an instance named +i_name+ with
+        # possible arguments +args+.
+        def instantiate(i_name,*args)
+            # Create the eigen type.
+            eigen = self.class.new("")
+            eigen.instance_eval(*args,&@instance_proc) if @instance_proc
+            # Create the instance.
+            return @instance_class.new(i_name,eigen)
         end
 
+        # Generates the instantiation capabilities including an instantiation
+        # method +name+ for hdl-like instantiation, target instantiation as
+        # +klass+, added to the calling object with +add_instance+, and
+        # whose eigen type is initialized by +block+.
+        def make_instantiater(name,klass,add_instance,&block)
+            # Set the instanciater.
+            @instance_proc = block
+            # Set the target instantiation class.
+            @instance_class = klass
+            # Set the hdl-like instantiation method.
+            HDLRuby::High.send(:define_method,name.to_sym) do |i_name,*args|
+                # Instantiate.
+                instance = self.instantiate(i_name,*args)
+                # Add the instance.
+                binding.receiver.send(add_instance,instance)
+            end
+        end
+
+        # Instantiate the hardware type 
     end
 
     ##
@@ -60,11 +113,27 @@ module HDLRuby::High
     end
 
 
+    # Classes describing hardware types.
+
     ## 
     # Describes a high-level system type.
     class SystemT < HDLRuby::Low::SystemT
         include HMix
         include HType
+
+        ##
+        # Creates a new high-level system type named +name+ and inheriting
+        # from +mixins+.
+        #
+        # The proc +block+ is executed when instantiating the system.
+        def initialize(name, *mixins, &block)
+            # Initialize the high-level system type.
+            super(name)
+            self.include(*mixins)
+            # Generate the instantiation command.
+            make_instantiater(name,SystemI,:add_systemI,&block)
+        end
+
     end
 
 
@@ -82,6 +151,15 @@ module HDLRuby::High
     # NOTE: behavior types do not support mixins!
     class BehaviorT < HDLRuby::Low::Behavior
         include HType
+
+        # Library of the existing behavior types.
+        BehaviorTs = { }
+        private_constant :BehaviorTs
+
+        # Get an existing behavior type by +name+.
+        def self.get(name)
+            return BehaviorTs[name.to_sym]
+        end
     end
 
 
@@ -99,14 +177,45 @@ module HDLRuby::High
     # Declares a high-level system type named +name+, with +includes+ mixins
     # hardware types and using +block+ for instantiating.
     def system(name, *includes, &block)
+        # print "system block=#{block}\n"
         # Creates the resulting system.
-        systemT = SystemT.new(name)
-        # Include the mixins.
-        systemT.include(*includes)
-        # Sets the proc used for instantiating if any.
-        systemT.instantiater = block if block
-        # Returns the resulting system.
-        return systemT
+        return SystemT.new(name,*includes,&block)
+    end
+
+
+    # Classes describing harware instances.
+
+
+    ##
+    # Describes a high-level system instance.
+    class SystemI < HDLRuby::Low::SystemI
+    end
+
+
+    ##
+    # Describes a high-level signal instance.
+    class SignalI < HDLRuby::Low::SystemI
+    end
+
+
+    ##
+    # Describes a high-level behavior instance.
+    class BehaviorI
+        # Creates a new behavior instance of behavior type +behaviorT+ named 
+        # +name+.
+        def initialize(name, behaviorT)
+            # Set the name as a symbol.
+            @name = name.to_sym
+            # Check and set the behaviorT.
+            if behaviorT.respond_to?(:to_sym) then
+                # The system is specified by name, get it.
+                behaviorT = BehaviorT.get(behaviorT.to_sym)
+            end
+            if !behaviorT.is_a?(SystemT) then
+                raise "Invalid class for a behavior type: #{behaviorT.class}"
+            end
+            @behaviorT = behaviorT
+        end
     end
 
 end
--- lib/HDLRuby/test_hruby_high.rb
+++ lib/HDLRuby/test_hruby_high.rb
@@ -12,19 +12,66 @@ $success = true
 
 print "Creating an empty system type... "
 begin
-   system :systemT0
-   $systemT0 = SystemT.get(:systemT0)
-   unless $systemT0 then
-       raise "Error: created system type not found."
-       $success =false
-   end
-   if $systemT0.name != :systemT0 then
-       raise "Error: invalid system type name, got #{$systemT0.name} but expecting systemT0."
-       $success = false
-   end
+    system :systemT0
+    $systemT0 = SystemT.get(:systemT0)
+    unless $systemT0 then
+        raise "Error: created system type not found."
+        $success =false
+    end
+    if $systemT0.name != :systemT0 then
+        raise "Error: invalid system type name, got #{$systemT0.name} but expecting systemT0."
+        $success = false
+    end
+    puts "Ok."
 rescue Exception => e
     puts "Error: unexpected exception raised #{e.inspect}\n"
     $success = false
+end
+
+print "Instantiate it... "
+begin
+    $systemI0 = $systemT0.instantiate("systemI0")
+    puts "Ok."
+rescue Exception => e
+    puts "Error: unexpected exception raised #{e.inspect}\n"
+    $success = false
+end
+
+print "\nCreating a system type including a system instance... "
+begin
+   system :systemT1 do
+       systemT0 :my_system
+   end
+   $systemT1 = SystemT.get(:systemT1)
+   unless $systemT1 then
+       raise "Error: created system type not found."
+       $success =false
+   end
+   if $systemT1.name != :systemT1 then
+       raise "Error: invalid system type name, got #{$systemT0.name} but expecting systemT0."
+       $success = false
+   end
+   puts "Ok."
+rescue Exception => e
+    puts "Error: unexpected exception raised #{e.inspect}\n"
+    $success = false
+end
+
+print "Instantiate it... "
+begin
+    $systemI1 = $systemT1.instantiate("systemI1")
+    systemI1Is = $systemI1.systemT.each_systemI.to_a
+    if systemI1Is.size != 1 then
+        puts "Error: invalid number of inner system instances, got #{systemI1Is.size} but expecting 1."
+        $success = false
+    elsif systemI1Is[0].name != :my_system then
+        puts "Error: invalid inner system instance, got #{systemI1Is[0].name} but expecting my_system."
+        $success = false
+    end
+    puts "Ok."
+# rescue Exception => e
+#     puts "Error: unexpected exception raised #{e.inspect}\n"
+#     $success = false
 end
 
 
