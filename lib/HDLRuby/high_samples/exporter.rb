cd109a9e3618f4df57509ba3f9edb63c1a7a03dc
Lovic Gauthier <lovic@ariake-nct.ac.jp>
2017-06-26 15:38:54 +0900

    Added a sample for checking export and inheritance.

---
+++ lib/HDLRuby/high_samples/exporter.rb
@@ -0,0 +1,36 @@
+require 'HDLRuby'
+include HDLRuby::High
+
+
+# A simple D-FF
+system :dff do
+    input :clk, :rst, :d
+    output :q
+
+    behavior(clk.posedge) { q <= d & ~rst }
+end
+
+# A system wrapping a D-FF and exporting things.
+system :exporter do
+    input :d
+    inner :clk, :rst
+
+    dff(:dff0).(clk: clk, rst: rst, d: d)
+
+    # Comment one of the following to check that without export, they cannot
+    # be used in a system inheriting from exporter
+    export :clk, :rst , :dff0 
+end
+
+# A system inheriting from exporter
+system :importer, exporter do
+    input :clk0, :rst0
+    output :q
+
+    clk <= clk0
+    rst <= rst0
+    dff0.q <= q
+end
+
+# Instantiate it for checking.
+Universe.importer :importerI
