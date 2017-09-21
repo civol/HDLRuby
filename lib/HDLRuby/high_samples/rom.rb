1a6ad974c6f578ff7c86f58b38218ead1dd8efe6
Lovic Gauthier <lovic@ariake-nct.ac.jp>
2017-09-21 20:33:19 +0900

    Fixed the rom sample.

--- lib/HDLRuby/high_samples/rom.rb
+++ lib/HDLRuby/high_samples/rom.rb
@@ -2,22 +2,24 @@ require 'HDLRuby'
 
 configure_high
 
-# Describes an 8-bit data 16-bit address ROM.
-system :rom8_16 do
-    [15..0].input :addr
+# Describes an 8-bit data 8-bit address ROM.
+system :rom8_8 do
+    [7..0].input :addr
     [7..0].inout :data
 
-    bit[7..0][2**16].inner :content
+    bit[7..0][2**8].inner :content
+
+    content <= (2**8).times.to_a
 
     data <= content[addr]
 end
 
 # Instantiate it for checking.
-rom8_16 :rom8_16I
+rom8_8 :rom8_8I
 
 
 # Generate the low level representation.
-low = rom8_16I.to_low
+low = rom8_8I.to_low
 
 # Displays it
 puts low.to_yaml
