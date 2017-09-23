4e9842b495846db21dff7e04e9a9dc70251673fc
Lovic Gauthier <lovic@ariake-nct.ac.jp>
2017-09-23 17:43:41 +0900

    Advanced the implementation of the library of counters.

--- lib/HDLRuby/std/counters.rb
+++ lib/HDLRuby/std/counters.rb
@@ -1,31 +1,63 @@
-
-module HDLRuby::High
+module HDLRuby::High::Counter
 
 ##
 # Standard HDLRuby::High library: counters
 # 
 ########################################################################
-
-    ## Set a counter to +init+ when +rst+ is 1 and that is decreased according
-    #  to the current clock.
-    #  When this counter reaches 0, +code+ is executed.
-    #  When not within a block, a behavior will be created which is activated
-    #  on the rising edge of clk
-    def after(init, rst = $rst, clk = $clk, &code)
+    
+    ## Sets a counter to +init+ when +rst+ is 1 that is decreased according
+    #  to +clk+.
+    #  +code+ will be applied on this counter.
+    #  When not within a block, a behavior will be created which is
+    #  activated on the rising edge of +clk+.
+    def with_counter(init, rst = $rst, clk = $clk, &code)
+        # Creates the counter
         counter = High.make_variable(:counter)
         # Are we in a block?
         if High.top_user.is_a?(High::SystemT) then
             # No, create a behavior.
             behavior(clk.posedge) do
-                after(init,rst&code)
+                with_counter(init,rst,clk,&code)
             end
-        else 
+        else
+            code.call(counter)
+        end
+    end
+
+    
+    ## Sets a counter to +init+ when +rst+ is 1 that is decreased according
+    #  to +clk+.
+    #  As long as this counter does not reach 0, +code+ is executed.
+    #  When not within a block, a behavior will be created which is
+    #  activated on the rising edge of +clk+.
+    def before(init, rest = $rst, clk = $clk, &code)
+        with_counter(init,rst,clk) do |counter|
             seq do
                 inner counter
-                ifh rst == 1 do
+                ifh(rst == 1) do
                     get_inner(counter) <= init
                 end
-                elsifh get_inner(counter) == 0 do
+                elsifh(get_inner(counter) != 0) do
+                    get_inner(counter) <= get_inner(counter) - 1
+                    code.call
+                end
+            end
+        end
+    end
+
+    ## Sets a counter to +init+ when +rst+ is 1 that is decreased according
+    #  to +clk+.
+    #  When this counter reaches 0, +code+ is executed.
+    #  When not within a block, a behavior will be created which is activated
+    #  on the rising edge of +clk+.
+    def after(init, rst = $rst, clk = $clk, &code)
+        with_counter(init,rst,clk) do |counter|
+            seq do
+                inner counter
+                ifh(rst == 1) do
+                    get_inner(counter) <= init
+                end
+                elsifh(get_inner(counter) == 0) do
                     code.call
                 end
                 elseh do
