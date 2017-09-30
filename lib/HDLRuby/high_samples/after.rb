66cf8743ab00ccc8fb9239b3cd49e7e882d6269a
Lovic Gauthier <lovic@ariake-nct.ac.jp>
2017-09-30 10:06:36 +0900

    Fixed a bug with block-inner signals when generation low-level description. Added absolute uniq name generation system. Complete the implemention of standard library's after and before statements. Added an high level sample for testing after.

---
+++ lib/HDLRuby/high_samples/after.rb
@@ -0,0 +1,28 @@
+require 'HDLRuby'
+
+configure_high
+
+
+require 'HDLRuby/std/counters'
+include HDLRuby::High::Std
+
+
+# A simple test of the after construct
+system :with_after do
+    input :clk,:rst
+    output :timeout
+
+    behavior(clk.posedge,rst.posedge) do
+        hif(rst == 1) { timeout <= 0 }
+        after(100,rst) { timeout <= 1 }
+    end
+end
+
+# Instantiate it for checking.
+with_after :with_afterI
+
+# Generate the low level representation.
+low = with_afterI.to_low
+
+# Displays it
+puts low.to_yaml
