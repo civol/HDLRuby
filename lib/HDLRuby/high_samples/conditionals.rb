d445f7cbba976efa831fb82ce11d8d614bc98285
Lovic Gauthier <lovic@ariake-nct.ac.jp>
2018-03-05 21:10:28 +0900

    Added a high sample for testing the conditionals.

---
+++ lib/HDLRuby/high_samples/conditionals.rb
@@ -0,0 +1,29 @@
+require 'HDLRuby'
+
+configure_high
+
+
+# A system for testing conditionals
+system :conditionals do
+    [15..0].input :x,:y
+    output :s
+
+    behavior do
+        hif x <= y do
+            s <= 0
+        end
+        helse do
+            s <= 1
+        end
+    end
+
+end
+
+# Instantiate it for checking.
+conditionals :conditionalsI
+
+# Generate the low level representation.
+low = conditionalsI.to_low
+
+# Displays it
+puts low.to_yaml
