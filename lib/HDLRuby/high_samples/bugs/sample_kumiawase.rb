a0fa89266a84a2c0bb01001b82df1e44755d8bdd
Lovic Gauthier <lovic@ariake-nct.ac.jp>
2018-04-14 16:44:13 +0900

    Added a sample in the bug section of high_samples and low_samples

---
+++ lib/HDLRuby/high_samples/bugs/sample_kumiawase.rb
@@ -0,0 +1,52 @@
+require 'HDLRuby'
+configure_high
+
+#組み合わせ回路
+system :led7seg do
+	[3..0].input :in0
+	[6..0].output :out
+	[6..0].inner :out
+	
+	behavior do
+		hcase(in0)
+			hwhen 0 do
+				out <= b7b0111111
+			end
+			hwhen 1 do
+				out <= b7b0000110
+			end
+			hwhen 2 do
+				out <= b7b1011011
+			end
+			hwhen 3 do
+				out <= b7b1001111
+			end
+			hwhen 4 do
+				out <= b7b1100110
+			end
+			hwhen 5 do
+				out <= b7b1111101
+			end
+			hwhen 6 do
+				out <= b7b1111101
+			end
+			hwhen 7 do
+				out <= b7b0000111
+			end
+			hwhen 8 do
+				out <= b7b1111111
+			end
+			hwhen 9 do
+				out <= b7b1100111
+			end
+			helse do 
+				out <= 0
+			end
+		
+		
+	end
+end
+
+led7seg :led7segI
+
+puts led7segI.to_low.to_yaml
