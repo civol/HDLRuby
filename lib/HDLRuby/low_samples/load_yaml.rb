a3d46abbf5b076656bcfc64ed693451ad376f1b1
lovic <lovic@ariake-nct.ac.jp>
2018-05-29 09:39:23 +0900

    Added a low sample. Now load_yaml displays the loaded file.

--- lib/HDLRuby/low_samples/load_yaml.rb
+++ lib/HDLRuby/low_samples/load_yaml.rb
@@ -1,6 +1,11 @@
-# Simple program for loading a yaml description of HDLRuby::Low hardware.
+# Simple program for loading a yaml description of HDLRuby::Low hardware
+# and displaying it once again.
 
 require 'HDLRuby'
 include HDLRuby::Low
 
 hardwares = HDLRuby::from_yaml(File.read($*[0]))
+
+
+# Displays it
+puts hardwares[-1].to_yaml
