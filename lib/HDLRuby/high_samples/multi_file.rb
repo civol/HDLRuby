7ff1c48b5429476b6335b7e7f94e881a08278945
lovic <lovic@ariake-nct.ac.jp>
2018-07-31 22:09:18 +0900

    Added the possibility to call configure_high multiple times (only the first one having effect), in order to ease the processing of multiple files. Added the corresponding sample.

---
+++ lib/HDLRuby/high_samples/multi_file.rb
@@ -0,0 +1,30 @@
+require 'HDLRuby'
+
+configure_high
+
+# Tests the inclusion of other files.
+
+require "adder.rb"
+
+
+# Two parallel adders
+system :adder16_2 do
+    [15..0].input :x0,:y0,:x1,:y1
+    [16..0].output :s0,:s1
+
+    inner[15..0] :temp
+    inner :carry
+
+    adder(:adder0).(x0,y0,s0)
+    
+    adder(:adder1).(x1,y1,s1)
+end
+
+# Instantiate it for checking.
+adder16_2 :adder16_2I
+
+# Generate the low level representation.
+low = adder16_2I.to_low
+
+# Displays it
+puts low.to_yaml
