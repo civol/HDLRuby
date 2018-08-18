be7f84f6087b5e1d29cf50cfb7e3faa0c2b1b3d9
lovic <lovic@ariake-nct.ac.jp>
2018-08-19 06:35:24 +0900

    Added sample for testing the overriding of operators.

--- lib/HDLRuby/high_samples/overload.rb
+++ lib/HDLRuby/high_samples/overload.rb
@@ -3,10 +3,23 @@ require 'HDLRuby'
 configure_high
 
 
+# A 16-bit type with overloaded addition (saturated addition).
+[15..0].typedef(:sat16)
+sat16.define_operator(:+) do |left,right|
+    [16..0].inner :res
+    [15..0].inner :tmp
+    seq do
+        tmp <= left
+        res <= tmp + right
+        hif (res[16]) { res <= 0xFFFF }
+    end
+    res
+end
+
 # A simple 16-bit adder
 system :adder do
-    [15..0].input :x,:y
-    [16..0].output :s
+    sat16.input :x,:y
+    [15..0].output :s
 
     s <= x + y
 end
