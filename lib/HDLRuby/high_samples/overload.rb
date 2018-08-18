4121eb32d444cab816eb0fe23561c8eb6711f54a
lovic <lovic@ariake-nct.ac.jp>
2018-08-19 05:33:03 +0900

    Added the overloading of operators and (re)definition of methods in types to be applied on corresponding expressions.

---
+++ lib/HDLRuby/high_samples/overload.rb
@@ -0,0 +1,21 @@
+require 'HDLRuby'
+
+configure_high
+
+
+# A simple 16-bit adder
+system :adder do
+    [15..0].input :x,:y
+    [16..0].output :s
+
+    s <= x + y
+end
+
+# Instantiate it for checking.
+adder :adderI
+
+# Generate the low level representation.
+low = adderI.systemT.to_low
+
+# Displays it
+puts low.to_yaml
