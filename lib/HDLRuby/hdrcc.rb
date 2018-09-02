7b8d907291fbd326c141a659e7e5f1356046e906
lovic <lovic@ariake-nct.ac.jp>
2018-09-02 10:50:38 +0900

    Fixed potential name conflics generated by hdrcc.

--- lib/HDLRuby/hdrcc.rb
+++ lib/HDLRuby/hdrcc.rb
@@ -113,8 +113,8 @@ if __FILE__ == $0 then
     include HDLRuby
 
     # Process the command line options
-    options = {}
-    optparse = OptionParser.new do |opts|
+    $options = {}
+    $optparse = OptionParser.new do |opts|
         opts.banner = "Usage: hdrcc.rb [options] <input hdr file> [<output file>]"
  
         opts.separator ""
@@ -126,19 +126,19 @@ if __FILE__ == $0 then
         opts.separator "Options:"
 
         opts.on("-y", "--yaml", "Output in YAML format") do |y|
-            options[:yaml] = y
+            $options[:yaml] = y
         end
         opts.on("-v", "--verilog","Output in Verlog HDL format") do |v|
-            options[:verilog] = v
+            $options[:verilog] = v
         end
         opts.on("-d", "--directory","Specify the base directory for loading the hdr files") do |d|
-            options[:directory] = d
+            $options[:directory] = d
         end
         opts.on("-t", "--top system", "Specify the top system to process") do|t|
-            options[:top] = t
+            $options[:top] = t
         end
         opts.on("-p", "--param x,y,z", "Specify the generic parameters") do |p|
-            options[:param] = p
+            $options[:param] = p
         end
         opts.on_tail("-h", "--help", "Show this message") do
             puts opts
@@ -163,51 +163,51 @@ if __FILE__ == $0 then
         opts.separator "hdrcc -y -t multer -p 16,16,32 multer_gen.hdr multer_gen.yaml"
 
     end
-    optparse.parse!
+    $optparse.parse!
 
-    # puts "options=#{options}"
+    # puts "options=#{$options}"
 
     # Check the compatibility of the options
-    if options[:yaml] && options[:verilog] then
+    if $options[:yaml] && $options[:verilog] then
         warn("Please choose EITHER YAML OR Verilog HDL output.")
         puts optparse.help()
     end
 
     # Get the the input and the output files.
-    input,output = $*
+    $input,$output = $*
     # Get the top system name if name.
-    top = options[:top].to_s
-    unless top == "" || (/^[_[[:alpha:]]][_\w]*$/ =~ top) then
+    $top = $options[:top].to_s
+    unless $top == "" || (/^[_[[:alpha:]]][_\w]*$/ =~ $top) then
         warn("Please provide a valid top system name.")
         exit
     end
     # Get the generic parameters if any.
-    params = options[:param].to_s.split(",")
+    $params = $options[:param].to_s.split(",")
 
 
-    if input == nil then
+    if $input == nil then
         warn("Please provide an input hdr file (or consult the help using the --help option.)")
         exit
     end
 
     # Load and process the hdr files.
-    options[:directory] ||= "./"
-    loader = HDRLoad.new(top,input,options[:directory].to_s,*params)
-    loader.read_all
-    loader.check_all
-    top_instance = loader.parse
+    $options[:directory] ||= "./"
+    $loader = HDRLoad.new($top,$input,$options[:directory].to_s,*$params)
+    $loader.read_all
+    $loader.check_all
+    $top_instance = $loader.parse
 
     # Open the output.
-    if output then
-        output = File.open(output,"w")
+    if $output then
+        $output = File.open(output,"w")
     else
-        output = $stdout
+        $output = $stdout
     end
 
     # Generate the result.
-    if options[:yaml] then
-        output << top_instance.to_low.systemT.to_yaml
-    elsif options[:verilog] then
+    if $options[:yaml] then
+        $output << $top_instance.to_low.systemT.to_yaml
+    elsif $options[:verilog] then
         warn("Verilog HDL output is not available yet... but it will be soon, promise!")
     end
 end
