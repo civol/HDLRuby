681c5a80e697998262c3d6ecd953e1423db0ca07
lovic <lovic@ariake-nct.ac.jp>
2018-10-13 12:59:01 +0900

    Added the possibility to use operator * for bulding tuples with identical types.

--- lib/HDLRuby/hdr_samples/tuple.rb
+++ lib/HDLRuby/hdr_samples/tuple.rb
@@ -7,8 +7,15 @@ end
 
 system :my_system do
    input :clk, :rst
-   [bit, bit].inner :sig
+   [bit, bit].inner :zig
+   (bit*2).inner :zag
    
    dff(:dff0).(clk: clk, rst: rst)
-   dff0.d <= sig[0]
+   dff(:dff1).(clk: clk, rst: rst)
+   dff(:dff2).(clk: clk, rst: rst)
+   dff(:dff3).(clk: clk, rst: rst)
+   dff0.d <= zig[0]
+   dff1.d <= zig[1]
+   dff2.d <= zag[0]
+   dff3.d <= zag[1]
 end
