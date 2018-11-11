bad4b0d501c7f0f235eceda363ba5e4224e8b7c9
lovic <lovic@ariake-nct.ac.jp>
2018-11-11 12:01:08 +0900

    Cosmetical changes of forward generic sample.

--- lib/HDLRuby/hdr_samples/neural/forward.rb
+++ lib/HDLRuby/hdr_samples/neural/forward.rb
@@ -34,24 +34,22 @@ system :forward do |typ,arch,samps,b_init,w_init,
     cap_ws = []
     ws = []
     arch[1..-1].each.with_index do |col,i|
-        # puts "col=#{col} i=#{i}"
         cap_bs << []
         bs << []
-        # The biase updates
+        # The biase update inputs and value outputs
         col.times do |j| 
-            cap_bs[-1] << typ.input(:"cap_b#{i+1}_#{j+1}")
-            # puts "cap_bs[-1][-1] class: #{cap_bs[-1][-1].class}"
-            bs[-1] << typ.output(:"b#{i+1}_#{j+1}")
+            cap_bs[-1] << ( typ.input  :"cap_b#{i+1}_#{j+1}" )
+            bs[-1] <<     ( typ.output :"b#{i+1}_#{j+1}"     )
         end
-        # The weigh updates
+        # The weigh update inputs and value outputs
         cap_ws << []
         ws << []
         col.times do |j0|
             cap_ws[-1] << []
             ws[-1] << []
             arch[i].times do |j1|
-                cap_ws[-1][-1] << typ.input(:"cap_w#{i+1}_#{j0+1}#{j1+1}")
-                ws[-1][-1] << typ.output(:"w#{i+1}_#{j0+1}#{j1+1}")
+                cap_ws[-1][-1] << ( typ.input  :"cap_w#{i+1}_#{j0+1}#{j1+1}" )
+                ws[-1][-1] <<     ( typ.output :"w#{i+1}_#{j0+1}#{j1+1}"     )
             end
         end
     end
@@ -60,7 +58,7 @@ system :forward do |typ,arch,samps,b_init,w_init,
     arch[1..-1].each.with_index do |col,i|
         as << []
         col.times do |j|
-            as[-1] << typ.output(:"a#{i+1}_#{j+1}")
+            as[-1] << ( typ.output :"a#{i+1}_#{j+1}" )
         end
     end
     # The output signals indicating the current NN input and expected
@@ -68,12 +66,12 @@ system :forward do |typ,arch,samps,b_init,w_init,
     ks = []
     arch[0].times do |i|
         # NN input
-        ks << typ.output(:"k#{i+1}")
+        ks << ( typ.output :"k#{i+1}" )
     end
     ts = []
     arch[-1].times do |i|
         # NN expected answer
-        ts << typ.output(:"t#{i+1}")
+        ts << ( typ.output :"t#{i+1}" )
     end
 
     ###
@@ -89,7 +87,7 @@ system :forward do |typ,arch,samps,b_init,w_init,
     arch[1..-1].each.with_index do |col,i|
         zs << []
         col.times do |j|
-            zs[-1] << typ.inner(:"z#{i+1}_#{j+1}")
+            zs[-1] << ( typ.inner :"z#{i+1}_#{j+1}" )
         end
     end
 
@@ -99,23 +97,25 @@ system :forward do |typ,arch,samps,b_init,w_init,
 
     # Sample counter
     counter(outT,samps[0][0].size).(:counterI).(clk, reset, out)
+
     # Neuron update selector, the skip size is (NN depth)*4+1
     # (4-cycle neurons and one addition sync cycle).
     selector(arch.size*4+1).(:selectorI).(clk, reset, select_update)
+
     # Input and expected output memories
     arch[0].times do |i|
-        # Input samples are the first ones.
+        # Samples for input i
         mem(typ,outT,samps[0][i]).(:"k#{i+1}I").(clk, din, out, ks[i])
     end
     arch[-1].times do |i|
-        # Expected output samples are the second ones.
+        # Expected values for output i
         mem(typ,outT,samps[1][i]).(:"t#{i+1}I").(clk, din, out, ts[i])
     end
+
     # Biases and weights
     arch[1..-1].each.with_index do |col,i|
         # Biases
         col.times do |j|
-            # puts "bs[i][j]=#{bs[i][j]}"
             bw(typ,b_init[i][j]).
                 (:"b#{i+1}_#{j+1}I").(clk, reset, cap_bs[i][j],
                     select_initial, select_update, bs[i][j])
@@ -123,19 +123,16 @@ system :forward do |typ,arch,samps,b_init,w_init,
         # Weights
         col.times do |j0|
             arch[i].times do |j1|
-            # puts "i=#{i} j0=#{j0} j1=#{j1}"
-            # puts "w_init[i][j1][j0]=#{w_init[i][j0][j1]}"
-            # puts "cap_ws[i][j0][j1]=#{cap_ws[i][j0][j1]}"
                 bw(typ,w_init[i][j0][j1]).
                     (:"w#{i+1}_#{j0+1}#{j1+1}I").(clk,reset,cap_ws[i][j0][j1],
                     select_initial, select_update, ws[i][j0][j1])
             end
         end
     end
-    # Sums
+
+    # Weighted Sums
     # First column
     arch[1].times do |j|
-        # puts "j=#{j} bs[0][j]=#{bs[0][j]} zs[0][j]=#{zs[0][j]}"
         z(typ,ks.size,sopP).(:"z2_#{j+1}I").
             (clk, reset, *ks, *ws[0][j], bs[0][j], zs[0][j])
     end
@@ -146,7 +143,8 @@ system :forward do |typ,arch,samps,b_init,w_init,
                 (clk, reset, *as[i], *ws[i+1][j], bs[i+1][j], zs[i+1][j])
         end
     end
-    # Activations.
+
+    # Activations
     arch[1..-1].each.with_index do |col,i|
         col.times do |j|
             a(typ,actP).(:"a#{i+1}_#{j+1}I").(clk, reset, zs[i][j], as[i][j])
