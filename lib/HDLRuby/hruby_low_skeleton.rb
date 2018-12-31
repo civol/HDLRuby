317652db7c4c618568b92394cd61bd3b121168a2
lovic <lovic@ariake-nct.ac.jp>
2018-12-31 17:16:32 +0900

    Added the list of the HDLRuby::Low classes to servef as basis for extension (just copy the file to your new extension).

---
+++ lib/HDLRuby/hruby_low_skeleton.rb
@@ -0,0 +1,129 @@
+require 'HDLRuby'
+
+
+module HDLRuby::Low
+
+
+##
+# Skeleton of the HRuby::Low class structure.
+#
+# Can be used as basis for extending HDLRuby::Low
+#
+########################################################################
+
+
+    class SystemT
+    end
+
+    class Scope
+    end
+
+    class Type
+    end
+
+    class TypeDef
+    end
+
+    class TypeVector
+    end
+
+    class TypeTuple
+    end
+
+    class TypeStruct
+    end
+
+    class Behavior
+    end
+
+    class TimeBehavior
+    end
+
+    class Event
+    end
+
+    class SignalI
+    end
+
+    class SystemI
+    end
+
+    class Statement
+    end
+
+    class Transmit
+    end
+    
+    class If
+    end
+
+    class When
+    end
+
+    class Case
+    end
+
+    class Delay
+    end
+
+    class TimeWait
+    end
+
+    class TimeRepeat
+    end
+
+    class Block
+    end
+
+    class TimeBlock
+    end
+
+    class Code
+    end
+
+    class Connection
+    end
+
+    class Expression
+    end
+
+    class Value
+    end
+
+    class Cast
+    end
+
+    class Operation
+    end
+
+    class Unary
+    end
+
+    class Binary
+    end
+
+    class Select
+    end
+
+    class Concat
+    end
+
+    class Ref
+    end
+
+    class RefConcat
+    end
+
+    class RefIndex
+    end
+
+    class RefRange
+    end
+
+    class RefName
+    end
+
+    class RefThis 
+    end
+
+end
