18f4b39402c08e53945dedf287bc1f771c866170
lovic <lovic@ariake-nct.ac.jp>
2019-01-03 18:01:28 +0900

    Now low_sample global tester also tests regeneration of HDLRuby::High code and generation of VHDL code.

--- lib/HDLRuby/low_samples/test_all.sh
+++ lib/HDLRuby/low_samples/test_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script for testing all the samples
 
+# Test general conversions
 for file in *.yaml
 do
     # Skip the samples starting by "_" (they are WIP)
@@ -14,6 +15,26 @@ do
     # Convert with port wires.
     # This conversion allows to check each_block_deep methods.
     bundle exec ruby port_maker.rb "$file" || exit 1
+done
+
+# Test HDLRuby output.
+for file in *.yaml
+do
+    # Skip the samples starting by "_" (they are WIP)
+    if [[ ${file:0:1} == "_" ]] ; then continue; fi
+    # A sample to check
+    bundle exec ruby yaml2hdr.rb "$file" || exit 1
+done
+
+# Test he VHDL output.
+for file in *.yaml
+do
+    # Skip the samples starting by "_" (they are WIP)
+    if [[ ${file:0:1} == "_" ]] ; then continue; fi
+    # Skip the vector.yaml sample since it contains a tuple not supported yet.
+    if [[ $file == "vector.yaml" ]] ; then continue; fi
+    # A sample to check
+    bundle exec ruby yaml2vhd.rb "$file" || exit 1
 done
 
 # Tests the cloner.
