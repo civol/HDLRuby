47ce1d16a7ca30e0eb0c209bc1f098cd8b15c076
lovic <lovic@ariake-nct.ac.jp>
2019-01-10 09:36:13 +0900

    Added advanced adder-subtracter samples.

---
+++ lib/HDLRuby/hdr_samples/addsubz.rb
@@ -0,0 +1,22 @@
+# An adder-suber
+system :addsubz do
+    [1..0].input  :opr
+    [15..0].input :x,:y
+    [16..0].output :s
+
+    # The only adder instance.
+    instance :add do
+        [15..0].input :x,:y
+        input :cin
+        [16..0].output :s
+        
+        s <= x+y+cin
+    end
+
+    # The control part for choosing between 0, add, sub and neg.
+    hcase(opr)
+    hwhen(0) { add.(0,0,0,s) }
+    hwhen(1) { add.(x,y,0,s) }
+    hwhen(2) { add.(x,~y,1,s) }
+    helse    { add.(0,~y,1,s) }
+end
---
+++ lib/HDLRuby/high_samples/addsubz.rb
@@ -0,0 +1,37 @@
+require 'HDLRuby'
+
+configure_high
+
+# An adder-suber
+system :addsub do
+    [1..0].input  :opr
+    [15..0].input :x,:y
+    [16..0].output :s
+
+    # The only adder instance.
+    instance :add do
+        [15..0].input :x,:y
+        input :cin
+        [16..0].output :s
+        
+        s <= x+y+cin
+    end
+
+
+    # Some computation.
+    hcase(opr)
+    hwhen(0) { add.(0,0,0,s) }
+    hwhen(1) { add.(x,y,0,s) }
+    hwhen(2) { add.(x,~y,1,s) }
+    helse    { add.(0,~y,1,s) }
+end
+
+
+# Instantiate it for checking.
+addsub :addsubI
+
+# Generate the low level representation.
+low = addsubI.systemT.to_low
+
+# Displays it
+puts low.to_yaml
