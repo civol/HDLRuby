893db01f97ff10619c4299e4d490d0ff958296bf
lovic <lovic@ariake-nct.ac.jp>
2019-08-27 11:52:43 +0900

    Commited obsolete generic_transmission.rb for removal.

--- lib/HDLRuby/high_samples/generic_transmission.rb
+++ lib/HDLRuby/high_samples/generic_transmission.rb
@@ -2,42 +2,89 @@ require 'HDLRuby'
 
 configure_high
 
-# Some very complex system which sends 8 bit values.
-system :systemA do
-    input :clk, :rst
-    [7..0].output :data
+require 'HDLRuby/std/fsm'
+require 'HDLRuby/std/channel'
+include HDLRuby::High::Std
 
-    par(clk.posedge) do
-        ( data <= mux(rst == 0, 0, data + 1) ).hif(data.can_write)
+# Some very complex system which sends 8 bit values.
+system :systemA do |ch|
+    input :clk, :rst
+    [7..0].inner :count
+    ch.output :tx
+
+    par(clk) do
+        tx.put? do
+            count <= (rst == 0).mux(0, count + 1) 
+            tx.put <= count
+        end
     end
 end
 
+# Alternative implementation without clock.
+system :systemAnc do |ch|
+    input :rst
+    [7..0].inner :count
+    ch.output :tx
+
+    tx.put? do
+        count <= (rst == 0).mux(0, count + 1) 
+        tx.put <= count
+    end
+end
 
 # Another extremly complex system which recieves 8 bit values.
-system :systemB do
+system :systemB do |ch|
     input :clk
-    [7..0].input :data
     output :result
+    ch.input :rx
 
-    par(clk.posedge) do
-        ( result <= mux(data == 0, 0, 1) ).hif(data.can_read)
+    par(clk) do
+        rx.get? do
+            result <= mux(rx.get == 0, 0, 1)
+        end
     end
 end
 
+
+# Alternative implementation without clock.
+system :systemBnc do |ch|
+    output :result
+    ch.input :rx
+
+    rx.get? do
+        result <= mux(rx.get == 0, 0, 1)
+    end
+end
 
 # A system connecting A to B directly.
 system :directAB do
-    input :clk
+    input :clk, :rst
     output :result
 
     [7..0].inner :a2b
 
-    systemA(:sysA,a2b).(clk: clk)
-    systemB(:sysB,a2b).(clk: clk, result: result)
+    systemA([7..0]).(:sysA).(clk: clk, rst: rst, tx: a2b)
+    systemB([7..0]).(:sysB).(clk: clk, result: result, rx: a2b)
 end
+
+# A system connecting Anc to Bnc directly.
+system :directABnc do
+    input :clk, :rst
+    output :result
+
+    [7..0].inner :a2b
+    a2b.define_method(:get?,&blk) { par(clk.posedge,&blk) }
+    a2b.define_method(:put?,&blk) { par(clk.posedge,&blk) }
+
+    systemA(a2b).(:sysA).(data: a2b, rst: rst, tx: a2b)
+    systemB(a2b).(:sysB).(data: a2b, result: result, rx: a2b)
+end
+
 
 # Instantiate it for checking.
 directAB :directABI
+
+
 
 
 # A system connecting A to B through a serial interface.
@@ -45,42 +92,45 @@ system :serialAB do
     input :clk, :rst
     output :result
 
-    [7..0].inner :bufA   # Buffer for serialization on A side
-    [2..0].inner :scntA  # Counter for serialization on A side
-    [7..0].inner :bufB   # Buffer for serialization on B side
-    [2..0].inner :scntB  # Counter for serialization on B side
+    inner :sdat              # Serial data line
+    # inner :rdy, :ack         # Serial control lines
+    # inner[4..0] :wr_cnt      # Serial write counter
+    # inner[4..0] :rd_cnt      # Serial read counter
 
-    inner :sdat          # Serial data line
+    { buf: [8], wr_cnt: [5], rdy: bit }.inner  :atb
+    atb.define_method(:put?,&blk) do
+        hif(wr_cnt==0) { blk.call; rdy <= 1 }
+        helsif(wr_cnt==8) { rdy <= 0 }
+    end
+    atb.define_method(:put) { buf }
 
-    # Handle the serial transmission A side.
+    { buf: [8], rd_cnt: [5], ack: bit }.inner  :bfa
+    bfa.define_method(:get?,&blk) do
+        hif(rd_cnt==8) { blk.call; ack <= 1 }
+        helsif(rd_cnt==0) {ack <= 0 }
+    end
+    bfa.define_method(:put) { buf }
+
+    # Handle the serial transmission writer side.
     par(clk.posedge) do
-        hif (rst) {
-            scntA <= 0
-        }
-        helse {
-            scntA <= scntA + 1
-            sdat <= bufA[scntA]
-        }
-    end
-
-    # Handle the serial transmission B side.
-    par(clk.negedge) do
-        hif rst do
-            scntB <= 0
-        end ; helse do
-            scntB <= scntB + 1
-            bufB[scntB] <= sdat
+        hif(rdy == 1 && wr_cnt < 8) do
+            sdat <= atb.buf[wr_cnt]
+            wr_cnt <= wr_cnt + 1
         end
+        helsif(rdy==0) { wr_cnt <= 0 }
     end
 
-    # Set that bufA can only written when scntA becomes 0
-    bufA.can_write = scntA == 7
+    # Handle the serial transmission reader side.
+    par(clk.negedge) do
+        hif(ack == 1 && rd_cnt > 0) do
+            bfa.buf[rd_cnt-1] <= sdat
+            rd_cnt <= rd_cnt - 1
+        end
+        helsif(ack==0) { rd_cnt <= 8 }
+    end
 
-    # Set that bufB can only read when scntB becomes 0
-    bufA.can_read = scntB == 7
-
-    systemA(:sysA,bufA).(clk: clk)
-    systemB(:sysB,bufB).(clk: clk)
+    systemA(atb).(:sysA).(data: a2b, rst: rst, tx: atb)
+    systemB(bfa).(:sysB).(data: a2b, result: result, rx: bfa)
 end
 
 
@@ -88,7 +138,7 @@ end
 serialAB :serialABI
 
 # Generate the low level representation.
-low = serialABI.systemT.to_low
+low = serialABI.to_low
 
 # Displays it
 puts low.to_yaml
