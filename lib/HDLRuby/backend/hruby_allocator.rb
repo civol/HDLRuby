a661e5a169b6eefae9d1073c1e5d27bc10d98e0f
lovic <lovic@ariake-nct.ac.jp>
2019-10-14 10:30:12 +0900

    Fixed the signal allocators for exteranl languages backend.

--- lib/HDLRuby/backend/hruby_allocator.rb
+++ lib/HDLRuby/backend/hruby_allocator.rb
@@ -28,10 +28,19 @@ module HDLRuby::Low
             @word = word.to_i
             # Initialize the allocation counter.
             @head = first
+            # Initialize the allocation table.
+            @table = {}
         end
 
         ## Allocates space for +signal+.
+        #  NOTE: if the signal is already allocated, returns the previous
+        #        allocation result.
         def allocate(signal)
+            # Has the signal been already allocated?
+            if @table.key?(signal) then
+                # Yes return the allocation result.
+                return @table[signal]
+            end
             # Get the size to allocate in word.
             size = signal.type.width / @word
             size += 1 unless signal.type.width % word == 0
@@ -42,7 +51,18 @@ module HDLRuby::Low
             # Ok, performs the allocation.
             res = @head
             @head += size
+            @table[signal] = res
             return res
+        end
+
+        ## Get the address of +signal+ if allocated.
+        def get(signal)
+            return @table[signal]
+        end
+
+        ## Iterate over the allocated signals and their corresponding address.
+        def each
+            @table.each
         end
     end
 
