f32f1c6e8c86631ff3d8a08303b47a4de193e1c1
lovic <lovic@ariake-nct.ac.jp>
2020-02-20 18:21:17 +0900

    Added a for testing the port declaration and connection through generic argument in the register sample.

--- lib/HDLRuby/high_samples/registers.rb
+++ lib/HDLRuby/high_samples/registers.rb
@@ -56,22 +56,31 @@ system :regn do |n|
 end
 
 # Instantiate it for checking.
-regn :regn8I,8
+# regn :regn8I,8
+regn(8).(:regn8I)
 
 
 # Describes a register of generic type.
+# NOTE: the input can be automatically connected from the generic argument.
 system :reg do |typ|
     input :clk, :rst
     typ.input :d
+    # Now ensure typ is a type.
+    typ = typ.type if typ.is_a?(HExpression)
     typ.output :q,:qb
+
 
     qb <= ~q
     (q <= d & [~rst]*typ.width).at(clk.posedge)
 end
 
 # Instantiate it for checking.
-reg :regbit8I, bit[7..0]
+# reg :regbit8I, bit[7..0]
+reg(bit[7..0]).(:regbit8I)
 
+# Instantiate it with infered type from connection.
+bit[8].inner :sig0
+reg(sig0).(:regSig0I)
 
 
 # Function generating the body of a register description.
@@ -104,8 +113,10 @@ end
 # Instantiate these systems for checking them.
 dff_body  :dff_bodyI
 reg8_body :reg8_bodyI
-regn_body :regn_bodyI, 8
-reg_body  :reg_bodyI, bit[7..0]
+# regn_body :regn_bodyI, 8
+regn_body(8).(:regn_bodyI)
+# reg_body  :reg_bodyI, bit[7..0]
+reg_body(bit[7..0]).(:reg_bodyI)
 
 
 # Function generating a register declaration.
@@ -123,7 +134,7 @@ end
 # Now let's generate the register declarations.
 make_reg(:dff_make) { bit }
 make_reg(:reg8_make){ bit[7..0] }
-make_reg(:regn_make){ |n| bit[n-1..0] }
+make_reg(:regn_make){ |n| bit[(n-1)..0] }
 make_reg(:reg_make) { |typ| typ }
 
 # Instantiate these systems for checking them.
