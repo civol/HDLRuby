1fa2535dc67206489d41c07c41069da5256fde7b
lovic <lovic@ariake-nct.ac.jp>
2020-04-22 17:27:00 +0900

    Added one element in the content of the roms (forgotten them) in the rom sample.

--- lib/HDLRuby/hdr_samples/rom.rb
+++ lib/HDLRuby/hdr_samples/rom.rb
@@ -4,8 +4,8 @@ system :rom4_8 do
     [2..0].input :addr
     [7..0].output :data0,:data1,:data2
 
-    bit[7..0][0..7].constant content0: [1,2,3,4,5,6,7]
-    bit[7..0][-8].constant content1: [1,2,3,4,5,6,7]
+    bit[7..0][0..7].constant content0: [0,1,2,3,4,5,6,7]
+    bit[7..0][-8].constant content1: [0,1,2,3,4,5,6,7]
     bit[7..0][-8].constant content2: (8).times.to_a
 
     data0 <= content0[addr]
