b58f6ed5b0a0cabb87f85d53909b90070c656169
lovic <lovic@ariake-nct.ac.jp>
2020-07-30 20:52:16 +0900

    Fixed infinite loop with code including multiple times fixpoint.

--- lib/HDLRuby/std/fixpoint.rb
+++ lib/HDLRuby/std/fixpoint.rb
@@ -15,51 +15,53 @@ module HDLRuby::High::Std
     def self.included(base)
         # Performs the previous included
         res = self.send(:_included_fixpoint,base)
-        # Now modify the Type class
-        ::HDLRuby::High::Type.class_eval do
-            # Saves the former type generation method.
-            alias_method :"_[]_fixpoint", :[]
+        # Now modify the Type class if not already modified.
+        unless ::HDLRuby::High::Type.instance_methods.include?(:"_[]_fixpoint") then
+            ::HDLRuby::High::Type.class_eval do
+                # Saves the former type generation method.
+                alias_method :"_[]_fixpoint", :[]
 
-            # Redefine the type generation method for supporting fixed point
-            # type generation.
-            def [](*args)
-                if args.size == 1 then
-                    return self.send(:"_[]_fixpoint",*args)
-                else
-                    # Handle the arguments and compute the fix point sizes.
-                    arg0,arg1 = *args
-                    if arg0.respond_to?(:to_i) then
-                        isize = arg0
+                # Redefine the type generation method for supporting fixed point
+                # type generation.
+                def [](*args)
+                    if args.size == 1 then
+                        return self.send(:"_[]_fixpoint",*args)
                     else
-                        isize = (arg0.first-arg0.last).abs+1
+                        # Handle the arguments and compute the fix point sizes.
+                        arg0,arg1 = *args
+                        if arg0.respond_to?(:to_i) then
+                            isize = arg0
+                        else
+                            isize = (arg0.first-arg0.last).abs+1
+                        end
+                        if arg1.respond_to?(:to_i) then
+                            fsize = arg1
+                        else
+                            fsize = (arg1.first-arg1.last).abs+1
+                        end
+                        # Build the type.
+                        case(self.name)
+                        when :bit
+                            typ = bit[isize+fsize].typedef(::HDLRuby.uniq_name)
+                        when :unsigned
+                            typ = unsigned[isize+fsize].typedef(::HDLRuby.uniq_name)
+                        when :signed
+                            typ = signed[isize+fsize].typedef(::HDLRuby.uniq_name)
+                        else
+                            raise "Invalid type for generating a fixed point type: #{self.name}"
+                        end
+                        # Redefine the multiplication and division for fixed point.
+                        typ.define_operator(:*) do |left,right|
+                            (left.as([isize+fsize*2])*right) >> fsize
+                        end
+                        typ.define_operator(:/) do |left,right|
+                            (left.as([isize+fsize*2]) << fsize) / right
+                        end
+                        typ
                     end
-                    if arg1.respond_to?(:to_i) then
-                        fsize = arg1
-                    else
-                        fsize = (arg1.first-arg1.last).abs+1
-                    end
-                    # Build the type.
-                    case(self.name)
-                    when :bit
-                        typ = bit[isize+fsize].typedef(::HDLRuby.uniq_name)
-                    when :unsigned
-                        typ = unsigned[isize+fsize].typedef(::HDLRuby.uniq_name)
-                    when :signed
-                        typ = signed[isize+fsize].typedef(::HDLRuby.uniq_name)
-                    else
-                        raise "Invalid type for generating a fixed point type: #{self.name}"
-                    end
-                    # Redefine the multiplication and division for fixed point.
-                    typ.define_operator(:*) do |left,right|
-                        (left.as([isize+fsize*2])*right) >> fsize
-                    end
-                    typ.define_operator(:/) do |left,right|
-                        (left.as([isize+fsize*2]) << fsize) / right
-                    end
-                    typ
                 end
+                return res
             end
-            return res
         end
     end
 
