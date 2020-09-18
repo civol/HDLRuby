29d0ff3f81eaafca99900a0818033fd27571d5f1
lovic <lovic@ariake-nct.ac.jp>
2020-09-18 10:38:46 +0900

    Extend test of fixpoint.

--- lib/HDLRuby/hdr_samples/with_fixpoint.rb
+++ lib/HDLRuby/hdr_samples/with_fixpoint.rb
@@ -9,6 +9,8 @@ system :fix_test do
 
     # Declare three 4-bit integer part 4-bit fractional part
     bit[3..0,3..0].inner :x,:y,:z
+    # Declare three 8-bit integer part 8-bit fractional part
+    signed[3..0,3..0].inner :a,:b,:c,:d
 
     # Performs calculation between then
     timed do
@@ -22,5 +24,12 @@ system :fix_test do
         !10.ns
         z <= z / x
         !10.ns
+        a <= _00010000
+        b <= _00001111
+        !10.ns
+        c <= a * b
+        d <= 0
+        !10.ns
+        d <= d + c 
     end
 end
