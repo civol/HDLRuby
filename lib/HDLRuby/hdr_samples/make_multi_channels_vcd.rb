6fbf71351296c22b2d2a25f2e288b31340360647
lovic <lovic@ariake-nct.ac.jp>
2020-11-16 20:30:25 +0900

    Updated sample testing multiple channels.

--- lib/HDLRuby/hdr_samples/make_multi_channels_vcd.rb
+++ lib/HDLRuby/hdr_samples/make_multi_channels_vcd.rb
@@ -3,23 +3,23 @@
 
 # The configuration scenarii
 $scenarii = [
-              [:sync,  :register],     # 00
-              [:sync,  :handshake],    # 01
-              [:sync,  :queue],        # 02
-              [:nsync, :register],     # 03
-              [:nsync, :handshake],    # 04
-              [:nsync, :queue],        # 05
-              [:async, :register],     # 06
-              [:async, :handshake],    # 07
-              [:async, :queue],        # 08
-              [:proco, :register],     # 09
-              [:proco, :handshake],    # 10
-              [:proco, :queue],        # 11
-              [:double,:register],     # 12
-              [:double,:handshake],    # 13 
-              [:double,:queue]         # 14
+              [:_clk2_clk2,      :register],    #  0
+              [:_clk2_nclk2,     :register],    #  1
+              [:_clk2_clk3,      :register],    #  2
+              [:_clk3_clk2,      :register],    #  3
+              [:_clk2_clk2,      :handshake],   #  4
+              [:_clk2_nclk2,     :handshake],   #  5
+              [:_clk2_clk3,      :handshake],   #  6
+              [:_clk3_clk2,      :handshake],   #  7
+              [:clk2_clk2_clk2,  :queue],       #  8
+              [:clk2_clk2_nclk2, :queue],       #  9
+              [:clk1_clk2_clk3,  :queue],       # 10
+              [:clk3_clk2_clk1,  :queue],       # 11
+              [:clk2_clk3_clk1,  :queue],       # 12
+              [:clk2_clk1_clk3,  :queue],       # 13
             ]
-(0..11).each do |i|
+$scenarii.each_with_index do |scenarii,i|
+    puts "scenario: [#{i}] #{scenarii}"
     `bundle exec ../hdrcc.rb -S --vcd with_multi_channels.rb WithMultiChannelPaper #{i}`
-    `mv WithMultiChannelPaper/hruby_simulator.vcd WithMultiChannelPaper/#{i.to_s.to_s.rjust(2,"0")}_#{$scenarii[i][0]}_#{$scenarii[i][1]}.vcd`
+    `mv WithMultiChannelPaper/hruby_simulator.vcd WithMultiChannelPaper/#{i.to_s.to_s.rjust(2,"0")}_#{scenarii[0]}_#{scenarii[1]}.vcd`
 end
--- lib/HDLRuby/hdr_samples/with_multi_channels.rb
+++ lib/HDLRuby/hdr_samples/with_multi_channels.rb
@@ -10,36 +10,51 @@ channel(:queue) do |typ,depth,clk,rst|
     # The read and write pointers.
     [depth.width].inner :rptr, :wptr
     # The read and write command signals.
-    inner :rcmd, :wcmd
+    inner :rreq, :wreq
     # The read and write ack signals.
     inner :rack, :wack
-    # The ack check deactivator (for synchron accesses).
-    inner :hrack, :hwack
     # The read/write data registers.
     typ.inner :rdata, :wdata
 
+    # The flags telling of the channel is synchronized
+    inner :rsync, :wsync
+
     # The process handling the decoupled access to the buffer.
     par(clk.posedge) do
-        # rack <= 0
-        # wack <= 0
-        hif (~rcmd) { rack <= 0 }
-        hif (~wcmd) { wack <= 0 }
         hif(rst) { rptr <= 0; wptr <= 0 }
-        hif(rcmd & (hrack|~rack) & (rptr != wptr)) do
-            rptr <= (rptr + 1) % depth
-            rack <= 1
-        end
-        hif(wcmd & (hwack|~wack) & (((wptr+1) % depth) != rptr)) do
-            buffer[wptr] <= wdata
-            # buffer[1] <= wdata
-            wptr <= (wptr + 1) % depth
-            wack <= 1
+        helse do
+            # hif(rsync) do
+            #     hif(rptr != wptr) do
+            #         rdata <= buffer[rptr]
+            #     end
+            # end
+            # helse do
+            hif(~rsync) do
+                hif (~rreq) { rack <= 0 }
+                hif(rreq & (~rack) & (rptr != wptr)) do
+                    rdata <= buffer[rptr]
+                    rptr <= (rptr + 1) % depth
+                    rack <= 1
+                end
+            end
+
+            # hif(wsync) do
+            #     buffer[wptr] <= wdata
+            # end
+            # helse do
+            hif(~wsync) do
+                hif (~wreq) { wack <= 0 }
+                hif(wreq & (~wack) & (((wptr+1) % depth) != rptr)) do
+                    buffer[wptr] <= wdata
+                    wptr <= (wptr + 1) % depth
+                    wack <= 1
+                end
+            end
         end
     end
-    par { rdata <= buffer[rptr] }
 
-    reader_output :rcmd, :rptr, :hrack
-    reader_input :rdata, :rack
+    reader_output :rreq, :rptr, :rsync
+    reader_input :rdata, :rack, :wptr, :buffer
 
     # The read primitive.
     reader do |blk,target|
@@ -47,66 +62,66 @@ channel(:queue) do |typ,depth,clk,rst|
             # Same clk event, synchrone case: perform a direct access.
             # Now perform the access.
             top_block.unshift do
-                rcmd <= 0
-                hrack <= 1
+                rsync <= 1
+                rreq <= 0
             end
             seq do
-                rptr <= (rptr + 1) % depth
-                target <= rdata
-                blk.call if blk
+                hif(rptr != wptr) do
+                    # target <= rdata
+                    target <= buffer[rptr]
+                    rptr <= (rptr + 1) % depth
+                    blk.call if blk
+                end
             end
         else
             # Different clk event, perform a decoupled access.
             top_block.unshift do
-                rcmd <= 0
-                hrack <= 0
-                hif(rack) do
-                    blk.call if blk
-                end
+                rsync <= 0
+                rreq <= 0
             end
-            seq do
-                # hif(rack) do
-                #     blk.call if blk
-                # end
-                # helse do
-                hif(rack==0) do
-                    rcmd <= 1 
+            par do
+                hif (~rack) { rreq <= 1 }
+                helsif(rreq) do
+                    rreq <= 0
                     target <= rdata
+                    blk.call if blk
                 end
             end
         end
     end
 
-    writer_output :wcmd, :wdata, :hwack
-    writer_input :wack
+    writer_output :wreq, :wdata, :wptr, :wsync, :buffer
+    writer_input :wack, :rptr
 
     # The write primitive.
     writer do |blk,target|
         if (cur_behavior.on_event?(clk.negedge,clk.posedge)) then
             # Same clk event, synchrone case: perform a direct access.
             top_block.unshift do
-                wcmd <= 0
-                hwack <= 1
+                wsync <= 1
+                wreq <= 0
             end
-            wcmd <= 1
-            wdata <= target
-            blk.call if blk
+            hif(((wptr+1) % depth) != rptr) do
+                # wdata <= target
+                buffer[wptr] <= target
+                wptr <= (wptr + 1) % depth
+                blk.call if blk
+            end
         else
             # Different clk event, asynchrone case: perform a decoupled access.
             top_block.unshift do
-                wcmd <= 0
-                hwack <= 0
-                hif(wack) do
-                    blk.call if blk
-                end
+                wsync <= 0
+                wreq <= 0
             end
             seq do
-                # hif(wack) do
-                #     blk.call if blk
-                # end
-                # helse 
-                hif(wack==0) { wcmd <= 1 }
-                wdata <= target
+                hif (~wack) do
+                    wreq <= 1
+                    wdata <= target
+                end
+                helsif(wreq) do
+                    wreq <= 0
+                    blk.call if blk
+                end
             end
         end
     end
@@ -181,82 +196,28 @@ channel(:handshake) do |typ|
     end
 end
 
-# Channel describing a handshake for transmitting data of +typ+ type, reset
-# by +rst+
-channel(:handshake2) do |typ|
-    # The data signal.
-    typ.inner :data
-    # The request and acknowledge.
-    inner :req, :ack
-    # The write flag
-    inner :wf
 
-    reader_input :ack, :data
-    reader_output :req
-
-    # The read primitive.
-    reader do |blk,target|
-        top_block.unshift do
-            req <= 0
-            hif(ack & req == 1) do
-                target <= data
-                req <= 0
-                blk.call if blk
-            end
-        end
-        hif(ack == 0) do
-            req <= 1
-        end
-    end
-
-    writer_input :req
-    writer_output :ack, :data
-    writer_inout :wf
-
-    # The read primitive.
-    writer do |blk,target|
-        top_block.unshift do
-            ack <= 0
-            hif(wf & req & ~ack == 1) do
-                data <= target
-                ack <= 1
-                blk.call if blk
-            end
-            hif(~req) { wf <= 0 }
-        end
-        hif(~ack) do
-            wf <= 1
-        end
-    end
-end
-
-
-# $mode = :sync
-# $mode = :nsync
-# $mode = :async
-# $mode = :proco  # Producer / Consummer
-# $mode = :double # Producer and Consummer with double channels.
+# $mode: channel clock, producer clock, consumer clock (n: not clock)
 # $channel = :register
 # $channel = :handshake
 # $channel = :queue
 
 # The configuration scenarii
 $scenarii = [
-              [:sync,  :register],     #  0
-              [:sync,  :handshake],    #  1
-              [:sync,  :queue],        #  3
-              [:nsync, :register],     #  4
-              [:nsync, :handshake],    #  5
-              [:nsync, :queue],        #  6
-              [:async, :register],     #  7
-              [:async, :handshake],    #  8
-              [:async, :queue],        #  9
-              [:proco, :register],     # 10
-              [:proco, :handshake],    # 11
-              [:proco, :queue],        # 12
-              [:double,:register],     # 13
-              [:double,:handshake],    # 14
-              [:double,:queue]         # 15
+              [:_clk2_clk2,      :register],    #  0
+              [:_clk2_nclk2,     :register],    #  1
+              [:_clk2_clk3,      :register],    #  2
+              [:_clk3_clk2,      :register],    #  3
+              [:_clk2_clk2,      :handshake],   #  4
+              [:_clk2_nclk2,     :handshake],   #  5
+              [:_clk2_clk3,      :handshake],   #  6
+              [:_clk3_clk2,      :handshake],   #  7
+              [:clk2_clk2_clk2,  :queue],       #  8
+              [:clk2_clk2_nclk2, :queue],       #  9
+              [:clk1_clk2_clk3,  :queue],       # 10
+              [:clk3_clk2_clk1,  :queue],       # 11
+              [:clk2_clk3_clk1,  :queue],       # 12
+              [:clk2_clk1_clk3,  :queue],       # 13
             ]
 
 # The configuration
@@ -266,113 +227,72 @@ puts "scenario: #{$scenarii[ARGV[-1].to_i]}"
 
 # Testing the queue channel.
 system :test_queue do
-    inner :clk, :rst, :clk2, :clk3
+    inner :rst, :clk1, :clk2, :clk3
     [8].inner :idata, :odata, :odata2
     [4].inner :counter
 
+
+    # Assign the clocks
+    mode = $mode.to_s.split("_")
+    if ($channel == :queue) then
+        clk_que = send(mode[0])
+    end
+    ev_pro = mode[1][0] == "n" ? 
+        send(mode[1][1..-1]).negedge : send(mode[1]).posedge
+    ev_con = mode[2][0] == "n" ? 
+        send(mode[2][1..-1]).negedge : send(mode[2]).posedge
+
+    # Set up the channel
     if $channel == :register then
         register(bit[8]).(:my_ch)
-        register(bit[8]).(:my_ch2)
     elsif $channel == :handshake then
         handshake(bit[8],rst).(:my_ch)
-        handshake(bit[8],rst).(:my_ch2)
     elsif $channel == :queue then
-        queue(bit[8],5,clk,rst).(:my_ch)
-        queue(bit[8],5,clk,rst).(:my_ch2)
+        queue(bit[8],3,clk_que,rst).(:my_ch)
     end
 
-    ev = $mode == :sync ? clk.posedge : 
-         $mode == :nsync ? clk.negedge : clk2.posedge
-
-    if $mode != :proco && $mode != :double then
-        # Sync/Neg sync and async tests mode
-        par(ev) do
-            hif(rst) do
-                counter <= 0
-                idata <= 0
-                # odata <= 0
-            end
-            helse do
-                hif (counter < 4) do
-                    my_ch.write(idata) do
-                        idata <= idata + 1
-                        counter <= counter + 1
-                    end
-                end
-                helsif ((counter > 10) & (counter < 15)) do
-                    my_ch.read(odata) do
-                        # idata <= idata - odata
-                        counter <= counter + 1
-                    end
-                end
-                helse do
-                    counter <= counter + 1
-                end
+    # Producter/consumer mode
+    # Producer
+    par(ev_pro) do
+        hif(rst) do
+            idata <= 0
+        end
+        helse do
+            my_ch.write(idata) do
+                idata <= idata + 1
             end
         end
-    elsif $mode == :proco then
-        # Producter/consumer mode
-        # Producer
-        par(clk2.posedge) do
-            hif(rst) do
-                idata <= 0
-            end
-            helse do
-                my_ch.write(idata) do
-                    idata <= idata + 1
-                end
-            end
+    end
+    # Consumer
+    par(ev_con) do
+        hif(rst) do
+            counter <= 0
         end
-        # Consumer
-        par(clk3.posedge) do
-            hif(rst) do
-                counter <= 0
-            end
-            helse do
-                my_ch.read(odata) do
-                    counter <= counter + 1
-                end
-            end
-        end
-    else
-        # Producer and consumer are commicating through two layers of channels
-        par(ev) do
-            hif(rst) do
-                counter <= 0
-                idata <= 0
-            end
-            helse do
-                my_ch.write(idata) do
-                    idata <= idata + 1
-                end
-                my_ch.read(odata) do
-                    my_ch2.write(odata)
-                end
-                my_ch2.read(odata2) do
-                    counter <= counter + 1
-                end
+        helse do
+            my_ch.read(odata) do
+                counter <= counter + 1
             end
         end
     end
 
     timed do
-        clk <= 0
+        clk1 <= 0
         clk2 <= 0
         clk3 <= 0
         rst <= 0
         !10.ns
-        clk <= 1
+        clk1 <= 1
         !10.ns
-        clk <= 0
+        clk1 <= 0
         rst <= 1
         !3.ns
         clk2 <= 1
         !3.ns
         clk3 <= 0
         !4.ns
-        clk <= 1
+        clk1 <= 1
         !10.ns
-        clk <= 0
+        clk1 <= 0
         !3.ns
         clk2 <= 0
         !3.ns
@@ -381,9 +301,9 @@ system :test_queue do
         rst <= 0
         !2.ns
         64.times do
-            clk <= 1
+            clk1 <= 1
             !10.ns
-            clk <= 0
+            clk1 <= 0
             !3.ns
             clk2 <= ~clk2
             !3.ns
