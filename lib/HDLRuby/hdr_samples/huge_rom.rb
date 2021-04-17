b669ef65bc1aef27b0bf1c4f7b95d58e7a4f4a25
lovic <lovic@ariake-nct.ac.jp>
2021-04-17 11:50:12 +0900

    Added a sample for testing huge constant initialization.

---
+++ lib/HDLRuby/hdr_samples/huge_rom.rb
@@ -0,0 +1,25 @@
+# Describes an 8-bit data 16-bit address ROM.
+system :huge_rom do
+    [15..0].input :addr
+    [7..0].output :data
+
+    bit[7..0][-65536].constant content: 65536.times.to_a
+
+    data <= content[addr]
+end
+
+
+
+system :test_rom do
+    [15..0].inner :addr
+    [7..0].inner :data
+
+    huge_rom(:my_rom).(addr,data)
+
+    timed do
+        8.times do |i|
+            addr <= i
+            !10.ns
+        end
+    end
+end
