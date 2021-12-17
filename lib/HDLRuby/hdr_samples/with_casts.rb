d6c02bb9c1dd27e800dd20e399a24ddd7385c347
lovic <lovic@ariake-nct.ac.jp>
2021-12-17 16:04:02 +0900

    Set the right code in with_casts.

--- lib/HDLRuby/hdr_samples/with_casts.rb
+++ lib/HDLRuby/hdr_samples/with_casts.rb
@@ -1,29 +1,26 @@
 
-def connect8(i0,i1,i2,i3,i4,i5,i6,i7,
-             o0,o1,o2,o3,o4,o5,o6,o7)
-    o0 <= i0
-    o1 <= i1
-    o2 <= i2
-    o3 <= i3
-    o4 <= i4
-    o5 <= i5
-    o6 <= i6
-    o7 <= i7
-end
-
-# A benchmark for testing the conversion to ruby array of expressions.
-system :with_to_bench do
-    [8].inner :val
-    inner :b0,:b1,:b2,:b3,:b4,:b5,:b6,:b7
-
-    connect8(*val,b0,b1,b2,b3,b4,b5,b6,b7)
+# A benchmark for testing the casts.
+system :with_cast do
+    [8].inner :count
+    [8].inner :val0,:val1
+    [9].inner :val2
 
     timed do
-        val <= _01101010
+        val0 <= _11111111
+        val1 <= _00000010
+        count <= 0
         !10.ns
-        val <= _01011010
+        count <= 1
+        val2 <= val0 + val1
         !10.ns
-        val <= _00001111
+        count <= 2
+        val2 <= val0.as(bit[9]) + val1
+        !10.ns
+        count <= 3
+        val2 <= (val0 + val1).as(bit[9])
+        !10.ns
+        count <= 4
+        val2 <= (val0 + val1).as(bit[8])
         !10.ns
     end
 end
