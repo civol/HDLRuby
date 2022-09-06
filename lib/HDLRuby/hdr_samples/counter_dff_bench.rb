f1a8eec61bb0dba288f22e594ca57aae767ce9a4
lovic <lovic@ariake-nct.ac.jp>
2022-09-06 16:39:29 +0900

    Modified counter_dff_bench sample to test usage of dff output as clock.

--- lib/HDLRuby/hdr_samples/counter_dff_bench.rb
+++ lib/HDLRuby/hdr_samples/counter_dff_bench.rb
@@ -1,79 +1,46 @@
 # A simple D-FF
 system :dff do
-    input  :d, :clk, :rst
-    output :q
+    input  :clk, :d
+    output q: 0
 
-    (q <= d & ~rst).at(clk.posedge)
+    (q <= d).at(clk.posedge)
 end
 
 # A benchmark for the dff.
 system :dff_bench do
-    inner :d0, :d1, :clk, :rst
-    inner :q0, :q1
+    inner :clk
+    inner :d0, :q0, :d1, :q1
 
-    dff(:my_dff0).(d0,clk,rst,q0)
-    dff(:my_dff1).(d1,clk,rst,q1)
+    dff(:my_dff0).(clk,d0,q0)
+    dff(:my_dff1).(d0,d1,q1)
+
+    d0 <= ~q0
+    d1 <= ~q1
 
     timed do
         clk <= 0
-        rst <= 0
-        d0  <= _z
-        d1  <= _z
         !10.ns
         clk <= 1
-        rst <= 0
-        d0  <= _z
-        d1  <= _z
         !10.ns
         clk <= 0
-        rst <= 1
-        d0  <= _z
-        d1  <= _z
         !10.ns
         clk <= 1
-        rst <= 1
-        d0  <= _z
-        d1  <= _z
         !10.ns
         clk <= 0
-        rst <= 0
-        d0  <= 1
-        d1  <= 0
         !10.ns
         clk <= 1
-        rst <= 0
-        d0  <= 1
-        d1  <= 0
         !10.ns
         clk <= 0
-        rst <= 0
-        d0  <= 1
-        d1  <= 1
         !10.ns
         clk <= 1
-        rst <= 0
-        d0  <= 1
-        d1  <= 1
         !10.ns
         clk <= 0
-        rst <= 0
-        d0  <= 0
-        d1  <= 1
         !10.ns
         clk <= 1
-        rst <= 0
-        d0  <= 0
-        d1  <= 1
         !10.ns
         clk <= 0
-        rst <= 0
-        d0  <= 0
-        d1  <= 0
         !10.ns
         clk <= 1
-        rst <= 0
-        d0  <= 0
-        d1  <= 0
         !10.ns
     end
 end
