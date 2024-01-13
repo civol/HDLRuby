0874112000e4bbe4deca5d44b050d9a679a6d302
lovic <lovic@ariake-nct.ac.jp>
2024-01-13 11:47:51 +0900

    Added the interface for software ruby programs.

---
+++ lib/rubyHDL.rb
@@ -0,0 +1,30 @@
+require 'hruby_sim/hruby_sim'
+
+##
+# Module for accessing HDLRuby hardware from a ruby program excuted on
+# an embedded processor: HDLRuby simulator version.
+########################################################################
+module RubyHDL
+
+    # Creates a new port 'name' assigned to signal 'sig'.
+    def self.port(name,sig)
+        # Create the accessing methods.
+        # For reading.
+        define_singleton_method(name.to_sym) do
+            RCSim.rcsim_get_signal_fixnum(sig)
+        end
+
+        # For writing.
+        defile_singleton_method(:"#{name}=") do |val|
+            RCSim.rcsim_transmit_fixnum_to_signal_seq(sig,val)
+        end
+    end
+
+    # Creates a new program 'name' assign to simulator code 'code'.
+    def self.program(name,code)
+        # Create the accessing method.
+        define_singleton_method(name.to_sym) do
+            code
+        end
+    end
+end
