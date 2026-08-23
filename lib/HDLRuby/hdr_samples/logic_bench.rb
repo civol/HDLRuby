a398b0733cfe7ae59a6108cfa94d8c49181acc35
Lovic Gauthier <lovic@ariake-nct.ac.jp>
2026-08-23 12:27:42 +0900

    Renamed nxor to eq since the behavior of == has been fixed.

--- lib/HDLRuby/hdr_samples/logic_bench.rb
+++ lib/HDLRuby/hdr_samples/logic_bench.rb
@@ -2,25 +2,25 @@
 # A benchmark for the logic operations.
 system :logic_bench do
     [3].inner :x,:y
-    [3].inner :s_not, :s_and, :s_or, :s_xor, :s_nxor
+    [3].inner :s_not, :s_and, :s_or, :s_xor, :s_eq
     
     signed[16].inner :a,:b,:shl,:shr
 
     timed do
         8.times do |i|
             8.times do |j|
-                x      <= i
-                y      <= j
-                s_not  <= ~x
-                s_and  <= x & y
-                s_or   <= x | y
-                s_xor  <= x ^ y
-                s_nxor <= (x == y)
+                x     <= i
+                y     <= j
+                s_not <= ~x
+                s_and <= x & y
+                s_or  <= x | y
+                s_xor <= x ^ y
+                s_eq  <= (x == y)
                 !10.ns
-                a      <= i
-                b      <= j
-                shl    <= (a << b)
-                shr    <= (a >> b)
+                a     <= i
+                b     <= j
+                shl   <= (a << b)
+                shr   <= (a >> b)
             end
         end
     end
